1f7f07f0d6e8b540cd85541ab0a4a79792378a18
Alex Turbin <turbin28@gmail.com>
2022-03-15 23:09:17 +0300

    test rout

---
+++ fullstack/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+//   res.send("Hello World!");
+  res.status(200).json(
+      {
+          message:'Working'
+      }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
