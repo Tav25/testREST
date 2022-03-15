5fca456de9c3cbf199bd1a1bae5328d53debcda3
Alex Turbin <turbin28@gmail.com>
2022-03-15 23:17:48 +0300

    add nodemon

--- fullstack/index.js
+++ fullstack/index.js
@@ -1,16 +1,6 @@
-const express = require("express");
+const app = require("./app");
 
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-//   res.send("Hello World!");
-  res.status(200).json(
-      {
-          message:'Working'
-      }
-  );
-});
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
