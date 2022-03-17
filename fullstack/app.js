39a3e8ae20f0d178d387fc117bdc7c40558310da
Alex Turbin <turbin28@gmail.com>
2022-03-17 10:22:55 +0300

    connect DB

--- fullstack/app.js
+++ fullstack/app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const app = express();
 
 const mongoose = require("mongoose");
+const keys = require("./config/keys");
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 
 mongoose
-  .connect("")
+  .connect('mongodb+srv://tav25:xqxZsLIdyFzvP6Vk@cluster0.vvahu.mongodb.net/restApi?retryWrites=true&w=majority')
   .then(() => console.log("MB Connect"))
-  .catch(() => console.log(error));
+  .catch(() => console.log('error'));
 
+
+
+////
 const authRoutes = require("./routes/auth");
 const postRoutes = require("./routes/posting");
 
---
+++ fullstack/config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // mongoURI: 'mongodb+srv://tav25:xqxZsLIdyFzvP6Vk@cluster0.vvahu.mongodb.net/restApi?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://tav25:<password>@cluster0.vvahu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
+
+// mongosh "mongodb+srv://cluster0.vvahu.mongodb.net/myFirstDatabase" --apiVersion 1 --username tav25
+// mongodb+srv://tav25:<password>@cluster0.vvahu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb://username:password@host:port/database
