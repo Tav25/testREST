8daab21c3c86f4d616db221508b737d64c599cc5
Alex Turbin <turbin28@gmail.com>
2022-03-17 13:16:14 +0300

    add test is canadidate

--- fullstack/controllers/auth.js
+++ fullstack/controllers/auth.js
@@ -6,14 +6,33 @@ module.exports.login = function (req, res, next) {
   });
 };
 
-module.exports.register = function (req, res, next) {
-  res.status(200).json({
-    register: 'from controller',
-  });
+module.exports.register = async function (req, res, next) {
+  // res.status(200).json({
+  //   register: 'from controller',
+  // });
 
-  const user = new User({
-    email: req.body.email,
-    password: req.body.password
-  })
-  user.save().then(()=> console.log('User create'))
+  // const user = new User({
+  //   email: req.body.email,
+  //   password: req.body.password
+  // })
+  // user.save().then(()=> console.log('User create'))
+
+  candidate = await User.findOne({ email: req.body.email });
+
+  if (candidate) {
+    console.log("cand yes");
+    res.status(409).json({ message: "User is already registered" });
+  } else {
+    // console.log("cand no");
+    const user = new User({
+      email: req.body.email,
+      password: req.body.password,
+    });
+    try{
+      await user.save().then(() => console.log("User create"));
+      res.status(201).json({ message: "User registered" });
+    }catch(e){
+      console.log("Error")
+    }
+  }
 };
