fa6e85ec6b599433c49a18c002d962a237ca4975
Alex Turbin <turbin28@gmail.com>
2022-03-17 12:54:26 +0300

    add model test connection

--- fullstack/controllers/auth.js
+++ fullstack/controllers/auth.js
@@ -1,12 +1,19 @@
+const User = require("../models/User");
+
 module.exports.login = function (req, res, next) {
   res.status(200).json({
     login: req.body,
   });
-}
-
+};
 
 module.exports.register = function (req, res, next) {
   res.status(200).json({
     register: 'from controller',
   });
-}
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  })
+  user.save().then(()=> console.log('User create'))
+};
